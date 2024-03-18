7892de2995622039a9e6a21b771661ef73daec7c
suvrat344 <88209093+suvrat344@users.noreply.github.com>
2024-03-18 22:43:38 +0530

    Problem Related To Linux Command

---
+++ script17.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Write two commands one on each line for the following two tasks.
+# Move only the file file_1 present in dir_1 to the empty directory dir_2.
+# Delete the directory dir_1.
+# dir_1 and dir_2 are directories in the current working directory. The operation should not change your current working directory.
+
+mv dir_1/file_1 dir_2
+rm -rf dir_1
