d59cc7de54b8a2b88491165d988faaef11c223e0
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-14 22:49:11 +0530

    Bash Scripting

--- BashScripting/script54.sh
+++ BashScripting/script54.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Write a bash script which takes one argument as the name of a file and prints Yes if the file has read permission only for the     # owner and no other permissions for owner or other users, else do not print anything. The file given in the argument will be presen # in the current working directory.
+# Write a bash script which takes one argument as the name of a file and prints Yes if the file has read permission only for the     owner and no other permissions for owner or other users, else do not print anything. The file given in the argument will be present  in the current working directory.
 
 if [[ $(ls -l $1 | grep "^-r--------*") ]]
 then
