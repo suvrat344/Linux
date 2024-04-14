c427e94652cb040c67b2a94b17e1783dae50d5b3
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-15 00:05:03 +0530

    Bash Scripting

--- BashScripting/script59g.sh
+++ BashScripting/script59g.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Write a bash script named ` script.sh ` that takes two arguments, checks if both the arguments are positive integers then prints   their sum; else prints "NOT INTEGERS" to STDERR and exits with exit code 1.
+# Write a bash script that takes two arguments, checks if both the arguments are positive integers then prints their sum; else prints"NOT INTEGERS" to STDERR and exits with exit code 1.
 # Note: Use the below if else conditional statement if needed.
 # ```bash
 # if condition; then
