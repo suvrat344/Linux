ee01abde68fcb85b61e0d7bea8695a210e45c506
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-15 00:11:11 +0530

    Bash Scripting

---
+++ BashScripting/script59j.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Write a bash script that takes an argument which takes a positive integer as argument and prints it in reverse order (For example, if the input number is 123, the Output will be 321)
+
+echo $1 | rev
