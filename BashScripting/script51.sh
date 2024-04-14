988c679b21100690eb0ea9c879b1e6218524010f
suvrat344 <88209093+suvrat344@users.noreply.github.com>
2024-04-14 22:16:33 +0530

    Bash Scripting

--- BashScripting/script51.sh
+++ BashScripting/script51.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Write a bash script that accepts any number of arguments and print the odd numbered arguments, i.e. first argument($1), third      argument($3), fifth argument($5) and so on.
+# Write a bash script that accepts any number of arguments and print the odd numbered arguments, i.e. first argument($1), third argument($3), fifth argument($5) and so on.
 # In the output the values of the arguments should be separated by a space and printed on the same line.
 
 index=1
