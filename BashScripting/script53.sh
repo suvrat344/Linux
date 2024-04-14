1f81c88ee4c64268f6f12a0bdde99e7da1fc66cf
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-14 22:15:00 +0530

    Bash Scripting

---
+++ BashScripting/script53.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Consider a file named data.txt in the current working directory. Write a script to determine if this file has more than 16 lines or# not. Your script should print Yes if the lines are more than 16; else print No.
+
+if [[ $(cat data.txt | wc -l) ]] 
+then
+	echo "Yes"
+else
+	echo "No"
+fi
