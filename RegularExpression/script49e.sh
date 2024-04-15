dddfb5def0c41e83e767d4f579a56f50a6ba8cc3
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-15 17:14:54 +0530

    File Used In Script49e.sh

---
+++ RegularExpression/script49e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The command below prints the file.
+# $ cat file.txt
+# India, New Delhi; Asia
+# Czech Republic, Prague; Europe
+# South Korea, Seoul; Asia
+# Uzbekistan, Tashkent; Asia
+# Mozambique, Maputo; Africa
+# United Arab Emirates, Abu Dhabi; Asia
+# Papua New Guinea, Port Moresby; Oceania
+# Estonia, Tallinn; Europe
+# Trinidad and Tobago, Port of Spain; South America
+# Ghana, Accra; Africa
+# Write a bash script to print all the countries in the file `file.txt` sorted alphabetically by name in reverse order.
+# (Hint: Use the command ` sort -r ` to sort in reverse order)
+
+cut -d "," -f1 file.txt | sort -r
