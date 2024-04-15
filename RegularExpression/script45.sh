370119d1e3e16d8cbb0e669cba1cc2c4d5157280
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-15 16:40:43 +0530

    Bash Scripting

---
+++ RegularExpression/script45.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Write a command that will print all the lines not containing the word gnu (case-insensitive) in the file test.txt present in the   current working directory.
+
+grep -vi "gnu" test.txt
