6fe3de5ce1eaee5987a0e1164e09f28eb99878bf
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-16 17:02:45 +0530

    Bash Scripting

---
+++ LinuxBasicCommand/script28.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# There is a file named `final.txt` in the current working directory, that contains some text that you do not want it to be edited.
+# Write a bash command to remove write permissions of user from this file. Do not edit any other file permissions
+
+chmod u-w final.txt
