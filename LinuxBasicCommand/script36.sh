cb140738dc5b1376327cbd043ac58c549440b612
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-16 17:59:21 +0530

    Bash Scripting

---
+++ LinuxBasicCommand/script36.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# There are three files master.txt,half1.txt and half2.txt in the current working directory. Add first 2 lines of half1.txt to the   file master.txt at the end(starting at a new line) then append the last 3 lines of the file half2.txt to the file master.txt at the  end(starting at a new line). Append the lines in the sequence mentioned.
+
+head -n2 half1.txt >> master.txt | tail -n3 half2.txt >> master.txt
