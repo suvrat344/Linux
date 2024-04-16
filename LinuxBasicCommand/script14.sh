683526290996cea2caf35e38e0693159b8d36fab
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-16 16:05:46 +0530

    Bash Scripting

---
+++ LinuxBasicCommand/script14.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# We have a file named "systemcommands.txt" in the present working directory. Write a Bash command to change its permissions to
+# user: read, write, execute
+# group: execute
+# others: write
+
+chmod 712 systemcommands.txt
