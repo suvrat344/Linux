fafc89ba4c702f86d7f081ea958316bb4a087091
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-16 17:12:39 +0530

    Bash Scripting

---
+++ LinuxBasicCommand/script29c.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Write a bash script which prints  the mime type of 'somefile' located in current directory.
+# Hint : Use 'file'.Refer manual of 'file' or 'file--help'.
+
+file --mime-type somefile
