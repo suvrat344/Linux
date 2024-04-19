445dac84886cf2ae7918f65cc04a4b443d64c7f6
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-19 16:58:34 +0530

    Bash Scripting

---
+++ Assignment/script4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create an account in GitHub and create a public repository named `se2001-<roll_number>`(in lowercase) and initialize with README.md
+# Create a file named `script4.sh` that exports two shell variables
+# - `GITHUB_USERNAME` that holds the GitHub username Ex: "bscse2001"
+# - `GITHUB_REPOSITORY` that holds the GitHub repository name Ex: "se2001-21f1000000"
+
+export GITHUB_USERNAME="suvrat344"
+export GITHUB_REPOSITORY="se2001-23f1002147"
