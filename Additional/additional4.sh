90e5cfddacb3fffbcbc604ab579478a531f47b4a
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-22 00:48:09 +0530

    Bash Scripting

---
+++ Additional/additional4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Print the previous login time of the user guest in the format MMM DD HH:MM:SS. Where MMM, DD, HH, MM and SS corresponds to Month   # (E.g. Nov), Date, Hours, Minutes and Seconds respectively. Extract the information from the logs available in the file myauth1.log in the current directory.
+
+grep "\bsession opened for user guest\b" myauth1.log | tail -1 | cut -d " " -f1-3
