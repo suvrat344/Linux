34ae7ae57dd89587352056dc53a18548c65699d0
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-22 00:56:51 +0530

    Bash Scripting

---
+++ Additional/additional8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Write a function `parent-of-parent` in the script  to return the parent of parent directory of the file/directory that is passed as# an argument.
+
+export file="home/suvrat344/week1/script1.sh"
+
+parent_of_parent(){
+	dirname "$(dirname "$file")"	
+}
+parent_of_parent
