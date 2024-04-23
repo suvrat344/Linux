bc32812a4f05495387298d63fbce299e93d0ed26
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-23 17:10:23 +0530

    Bash Scripting

---
+++ SedScripting/script64.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# Write a sed command to print the count of lines that starts with a digit in the file input.txt.Assume that there is at least one   line in the file input.txt that starts with a digit. Do not use the commands wc or awk , or even these keywords in comments or       anywhere in your answer.
+
+sed -n '/^[[:digit:]]/p' input.txt | sed -n '$='
