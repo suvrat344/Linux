9956836b057a70d99ec19eddeaebfa9a095c5bbe
Suvrat344 <chauhansuvrat344@gmail.com>
2024-04-23 17:13:25 +0530

    Bash Scripting

--- SedScripting/script61.sh
+++ SedScripting/script61.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Given a file words.txt containing a string in each line in the format FIRST_second. Every string is a combination of two words     joined with an underscore(_),the first word FIRST consists of all uppercase letters and the second word second consists of all       lowercase letters. Write a bash command/script using sed to convert all the string to SECOND_first.
+# Given a file words.txt containing a string in each line in the format FIRST_second. Every string is a combination of two words     # joined with an underscore(_), the first word FIRST consists of all uppercase letters and the second word second consists of all    # lowercase letters. Write a bash command/script using sed to convert all the string to SECOND_first.
 # After conversion
 # - The first and the second words should be swapped.
 # - The uppercase word should be converted to lowercase word and vice versa.
